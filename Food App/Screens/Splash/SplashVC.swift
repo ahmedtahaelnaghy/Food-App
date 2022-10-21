b1ee8c3205030858392a695def245cea359fc9cb
Ahmed Taha <ahmed.taha8399@gmail.com>
2022-10-21 16:57:05 +0200

    Configure the base application files and remove the main storyboard.

--- Food App/Screens/Splash/SplashVC.swift
+++ Food App/Screens/Splash/SplashVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashVC.swift
 //  Food App
 //
 //  Created by Ahmed Taha on 21/10/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
